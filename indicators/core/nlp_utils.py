a4f4a56eb62f078cc0e48733ea551b31feb848ce
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-02-24 15:00:47 +0000

    tests and docs

--- indicators/core/nlp_utils.py
+++ indicators/core/nlp_utils.py
@@ -12,33 +12,48 @@ import pickle
 import pandas as pd
 from nesta.packages.nlp_utils.ngrammer import Ngrammer
 from sklearn.feature_extraction.text import CountVectorizer
-from indicators.core.config import mysql_config
+from indicators.core.config import mysqldb_path
 
 
 def join_text(*args):
     """Concatenate all text arguments together, ignore None values.
 
     Args:
-        args (tuple): Any sequence of str or None values.
+      *args: All str arguments to be joined
+
     Returns:
-        joined: All text values concatenated together with a single space token.
+      joined: Single string representing the text
     """
-    return ' '.join(filter(None, args))
+    return " ".join(filter(None, args))
+
+
+def join_and_filter_sent(sent, extra_stops):
+    """Concatenate list, ignore stop terms.
+
+    Args:
+      sent (list): List of terms representing a sentence
+      extra_stops (list): Stop terms to be omitted from the sentence
+
+    Returns:
+      joined: Single string representing the sentence
+    """
+    _filter = filter(lambda term: term not in extra_stops, sent)
+    return join_text(*_filter)
 
 
 def join_doc(doc, extra_stops=[]):
-    """Join and conservatively lemmatise a term-wise document
+    """Join a term-wise document (list of list of terms)
 
     Args:
-        doc (list of list): Document of terms to be joined and lemmatised.
+      doc (list of list): Document of terms to be joined.
+      extra_stops: Extra stop terms to be removed from the document (Default value = [])
 
     Returns:
-        _doc: Joined and lemmatised documents
+      _doc: Joined and lemmatised documents
     """
-    term_filterer = lambda term: term not in extra_stops
-    sent_joiner = lambda sent: join_text(*filter(term_filterer, sent))
-    joined_sents = map(sent_joiner, doc)
-    joined_doc = '\n'.join(joined_sents)
+
+    joined_sents = map(lambda doc: join_and_filter_sent(doc, extra_stops), doc)
+    joined_doc = "\n".join(joined_sents)
     return joined_doc
 
 
@@ -47,13 +62,17 @@ def vectorise_docs(docs, min_df=10, max_df=0.95, extra_stops=[]):
     count vectoriser.
 
     Args:
-        docs (list): List of text documents to process.
+      docs(list): List of text documents to process.
+      min_df: (Default value = 10)
+      max_df: (Default value = 0.95)
+      extra_stops: (Default value = [])
 
     Returns:
-        (doc_vectors, feature_names): Vectorised documents and a list of feature names.
+      doc_vectors: Vectorised documents and a list of feature names.
+
     """
     # Process the text
-    ngrammer = Ngrammer(config_filepath=mysql_config, database="production")
+    ngrammer = Ngrammer(config_filepath=mysqldb_path, database="production")
     docs = [ngrammer.process_document(doc) for doc in docs]
     # Join and conservatively lemmatise
     docs = map(lambda doc: join_doc(doc, extra_stops), docs)
@@ -62,67 +81,80 @@ def vectorise_docs(docs, min_df=10, max_df=0.95, extra_stops=[]):
     doc_vectors = vec.fit_transform(docs)
     return doc_vectors, vec.get_feature_names()
 
-# %%
 
-
-def fit_topics(dataset_label, doc_vectors, feature_names, titles, n_topics,
-               anchors, anchor_strength=10, max_iter=25):
-    """[summary]
+def fit_topics(
+    dataset_label,
+    doc_vectors,
+    feature_names,
+    titles,
+    n_topics,
+    anchors,
+    anchor_strength=10,
+    max_iter=25,
+):
+    """Apply Corex topic modelling to a set of document vectors,
+    and save the model and output to disk.
 
     Args:
-        dataset_label ([type]): [description]
-        doc_vectors ([type]): [description]
-        feature_names ([type]): [description]
-        titles ([type]): [description]
-        n_topics ([type]): [description]
-        anchors ([type]): [description]
-        anchor_strength (int, optional): [description]. Defaults to 10.
-        max_iter (int, optional): [description]. Defaults to 25.
+      dataset_label(str): Name of this dataset, for labelling the output files
+      doc_vectors(np.array): Count (or equivalent) vector repr of the documents.
+      feature_names(list): Names of each feature in the doc_vectors
+      titles(list): Name of each document in doc_vectors
+      n_topics(int): Number of topics for Corex to generate
+      anchors(list of list): Corex anchor terms
+      anchor_strength(int, optional): Corex anchor strength multiplier. Defaults to 10.
+      max_iter(int, optional): Number of model iterations. Defaults to 25.
 
     Returns:
-        [type]: [description]
+      topic_model: trained Corex topic model
     """
-    kwargs = dict(X=doc_vectors, words=feature_names, docs=titles,
-                  anchors=anchors, anchor_strength=anchor_strength)
-    label = f'topic-model-{dataset_label}-{n_topics}-{max_iter}'
-
     topic_model = ct.Corex(max_iter=max_iter, n_hidden=n_topics)
-    topic_model.fit(**kwargs)
+    topic_model.fit(
+        X=doc_vectors,
+        words=feature_names,
+        docs=titles,
+        anchors=anchors,
+        anchor_strength=anchor_strength,
+    )
+    # Use Corex tools for writing the data to the local directory
+    label = f"topic-model-{dataset_label}-{n_topics}-{max_iter}"
     vt.vis_rep(topic_model, column_label=feature_names, prefix=label)
-    with open(label + '/model.pickle', 'wb') as f:
+    os.remove(f"{label}/cont_labels.txt")  # Very large file that we don't use
+    # pickle model for later use
+    with open(f"{label}/model.pickle", "wb") as f:
         pickle.dump(topic_model, f)
-    os.remove(label + '/cont_labels.txt')  # Very large file that we don't use
     return topic_model
-# %%
 
 
-def parse_topic(line, n_most=5):
-    """[summary]
+def parse_topic(raw_topic, n_most):
+    """Convert a verbose Corex topic into a concise human-readable form.
 
     Args:
-        line ([type]): [description]
-        n_most (int, optional): [description]. Defaults to 5.
+      raw_topic(str): Verbose Corex topic
+      n_most(int): Maximum number of terms to extract from the topic
 
     Returns:
-        [type]: [description]
+      topic_content(str): Concise human-readable Corex topic
     """
-    _, topic_content = line.split(":")
-    topic_content = ' '.join(topic_content.split(",")[:-n_most])
+    _, topic_content = raw_topic.split(":")
+    topic_content = " ".join(topic_content.split(",")[:n_most])
     return topic_content
 
 
-def parse_corex_topics(path):
-    """[summary]
+def parse_corex_topics(path, n_most=5):
+    """Parse concise human-readable topics from the output
+    of a Corex topic model.
 
     Args:
-        path ([type]): [description]
+      path(path-like): Path to the Corex topic model output
+      n_most(int): (Default value = 5)
 
     Returns:
-        [type]: [description]
+      topics(list): List of concise human-readable topics
     """
     topics = []
     with open(path) as f:
-        for line in f.readlines():
-            topic_content = parse_topic(line)
+        for raw_topic in f.readlines():
+            topic_content = parse_topic(raw_topic, n_most=n_most)
             topics.append(topic_content)
     return topics
