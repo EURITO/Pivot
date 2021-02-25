cb1cb8d09b1921badcb11f589ff0a1e925489a2d
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-02-25 15:38:38 +0000

    refactored

--- indicators/two/arxiv_topics.py
+++ indicators/two/arxiv_topics.py
@@ -3,6 +3,7 @@
 
 from collections import defaultdict
 from functools import lru_cache
+from itertools import groupby
 
 from indicators.core.config import EU_COUNTRIES
 from indicators.core.db import get_mysql_engine
@@ -15,6 +16,7 @@ from nesta.core.orms.orm_utils import db_session
 
 
 def get_arxiv_eu_insts():
+    """ """
     engine = get_mysql_engine()
     with db_session(engine) as session:
         # Join institutes to articles via the link table
@@ -29,7 +31,46 @@ def get_arxiv_eu_insts():
         # Group by, in order to deduplicate institutes
         q = q.group_by(Inst.id)
         # Make the request
-        return q.all()
+        return list(q.all())
+
+
+def make_reverse_lookup(data, key, prefix=""):
+    """
+
+    Args:
+      data:
+      key:
+      prefix:  (Default value = "")
+
+    Returns:
+
+    """
+    grouped_by_code = groupby(data, key=key)
+    # Look nuts code --> article id
+    return {
+        f"{prefix}{code}": set(id for id, _ in group)
+        for code, group in grouped_by_code
+        if code is not None
+    }
+
+
+def get_iso2_article_lookup(nuts_lookup):
+    """
+
+    Args:
+      nuts_lookup:
+
+    Returns:
+
+    """
+    engine = get_mysql_engine()
+    with db_session(engine) as session:
+        q = session.query(Link.article_id, Inst.country_code)
+        q = q.join(Link, Link.institute_id == Inst.id, isouter=True)
+        iso2_reverse = make_reverse_lookup(
+            q.all(), key=lambda id_code: id_code[1], prefix="iso_"
+        )
+    return iso2_reverse
 
 
 @lru_cache()
@@ -39,24 +80,19 @@ def get_arxiv_geo_lookup():
     Returns:
         [type]: [description]
     """
+    # Forward lookup
     nf = NutsFinder()
-    nuts_lookup = {
+    id_to_nuts_lookup = {
         id: nf.find(lat=lat, lon=lon) for id, lat, lon in get_arxiv_eu_insts()
     }
-    nuts_reverse = defaultdict(set)
-    engine = get_mysql_engine()
-    with db_session(engine) as session:
-        q = session.query(Link.article_id, Inst.country_code)
-        q = q.join(Link, Link.institute_id == Inst.id, isouter=True)
-        for id, code in q.all():
-            if code is None:
-                continue
-            nuts_reverse[f"iso_{code}"].add(id)
-    for id, nuts_info in nuts_lookup.items():
-        for nuts_region in nuts_info:
-            nuts_id = nuts_region["NUTS_ID"]
-            nuts_reverse[nuts_id].add(id)
-    return nuts_reverse
+    # Reverse lookups
+    nuts_to_id_lookup = make_reverse_lookup(
+        id_to_nuts_lookup.items(), key=lambda item: item[1]["NUTS_ID"]
+    )
+    iso2_to_id_lookup = get_iso2_article_lookup()
+    # Combine lookups and return
+    nuts_to_id_lookup.update(**iso2_to_id_lookup)
+    return nuts_to_id_lookup
 
 
 @lru_cache()
