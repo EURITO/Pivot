a609d507f8d3bf3d0c0475488d33fef0d75fdadc
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-03-10 10:37:41 +0000

    pre filter rather than post filter

--- indicators/core/nuts_utils.py
+++ indicators/core/nuts_utils.py
@@ -11,6 +11,7 @@ from indicators.core.config import NUTS_EDGE_CASES
 from itertools import groupby
 from operator import itemgetter
 from nuts_finder import NutsFinder as _NutsFinder
+import logging
 
 
 @lru_cache()
@@ -26,6 +27,7 @@ def NutsFinder():
 @lru_cache()
 def get_nuts_info_lookup():
     """Generate a lookup table of nuts ID to nuts info (name, level, code)"""
+    logging.info("Generating global NUTS lookup")
     nf = NutsFinder()
     lookup = {
         item["properties"]["NUTS_ID"]: {
@@ -82,10 +84,10 @@ def make_reverse_lookup(data, key=itemgetter(1), prefix=""):
        Grouped object of the form {code: {id1, id2, id3}}
     """
     # Look nuts code --> article id
+    data_wout_null_codes = filter(key, data)  # without null codes
     return {
         f"{prefix}{code}": set(id for id, _ in group)
-        for code, group in groupby(sorted(data, key=key), key=key)
-        if code is not None  # Not interested in null codes
+        for code, group in groupby(sorted(data_wout_null_codes, key=key), key=key)
     }
 
 
