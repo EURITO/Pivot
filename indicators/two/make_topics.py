45e39fc21a4020b81806702d56796628d67d05f5
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-03-31 15:27:33 +0100

    uncommented topic modules

--- indicators/two/make_topics.py
+++ indicators/two/make_topics.py
@@ -2,8 +2,8 @@ from indicators.core.nlp_utils import fit_topic_model
 from indicators.two import arxiv_topics, nih_topics, cordis_topics
 
 for module in (
-    # arxiv_topics,
-    # nih_topics,
+    arxiv_topics,
+    nih_topics,
     cordis_topics,
 ):  # Can append new modules as they arise
     fit_topic_model(module)
